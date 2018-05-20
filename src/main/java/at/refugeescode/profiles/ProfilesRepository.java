2e507128162172ccb3464fe9bfbed39db6d28439
ismael1987 <asmael-1987@hotmail.com>
2018-05-20 19:12:42 +0200

    the first add to this app

---
+++ src/main/java/at/refugeescode/profiles/ProfilesRepository.java
@@ -0,0 +1,6 @@
+package at.refugeescode.profiles;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfilesRepository extends JpaRepository <Participant, Long>{
+}
